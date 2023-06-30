b39b2f559db51c383135196f6cb8deebfd4d8996
pankaj <pankaj@geekinformatic.com>
2023-06-30 17:59:55 +0530

    change for testing

--- routes/web.php
+++ routes/web.php
@@ -13,6 +13,8 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::get('{any}', function () {
-    return view('/layout/app');
-})->where('any', '.*'); 
+//for testing purpose
+
+// Route::get('{any}', function () {
+//     return view('/layout/app');
+// })->where('any', '.*'); 
